0d8b3e37ef47e04853d28f1c475d46e0a38ada49
ben0109 <benjamin.leperchey@gmail.com>
2012-03-18 09:47:01 +0100

    glue cleanup

--- vhdl/src/bootloader_rom.vhd
+++ vhdl/src/bootloader_rom.vhd
@@ -5,11 +5,11 @@ Library UNISIM;
 use UNISIM.vcomponents.all;
 
 entity boot_rom is
-	port (clk	: in   STD_LOGIC;
-			RD_n	: in   STD_LOGIC;
-			A		: in   STD_LOGIC_VECTOR (12 downto 0);
-			D_in	: in   STD_LOGIC_VECTOR (7 downto 0);
-			D_out	: out  STD_LOGIC_VECTOR (7 downto 0));
+	port (
+		clk:		in  STD_LOGIC;
+		RD_n:		in  STD_LOGIC;
+		A:			in  STD_LOGIC_VECTOR (12 downto 0);
+		D_out:	out STD_LOGIC_VECTOR (7 downto 0));
 end entity;
 
 architecture Behavioral of boot_rom is
@@ -19,7 +19,7 @@ begin
       DO => D_out(1 downto 0),
       ADDR => a,
       CLK => not clk,
-      DI => D_in(1 downto 0),
+      DI => "00",
       EN => '1',
       SSR => '0',
       WE => '0'
@@ -30,7 +30,7 @@ begin
       DO => D_out(3 downto 2),
       ADDR => a,
       CLK => not clk,
-      DI => D_in(3 downto 2),
+      DI => "00",
       EN => '1',
       SSR => '0',
       WE => '0'
@@ -41,7 +41,7 @@ begin
       DO => D_out(5 downto 4),
       ADDR => a,
       CLK => not clk,
-      DI => D_in(5 downto 4),
+      DI => "00",
       EN => '1',
       SSR => '0',
       WE => '0'
@@ -52,7 +52,7 @@ begin
       DO => D_out(7 downto 6),
       ADDR => a,
       CLK => not clk,
-      DI => D_in(7 downto 6),
+      DI => "00",
       EN => '1',
       SSR => '0',
       WE => '0'
--- vhdl/src/main.vhd
+++ vhdl/src/main.vhd
@@ -29,44 +29,12 @@ end main;
 architecture Behavioral of main is
 
 	component clock is
-   port (CLKIN_IN			: in  std_logic; 
-			CLKIN_IBUFG_OUT: out std_logic;         
-			CLKFX_OUT 		: out std_logic;
-			CLKFX180_OUT	: out std_logic;
-			CLK2X_OUT		: out std_logic);
-	end component;
-	
-	component glue is
-	port (clk				: in  STD_LOGIC;
-			IO_n				: in  STD_LOGIC;
-			RD_n				: in  STD_LOGIC;
-			WR_n				: in  STD_LOGIC;
-			A					: in  STD_LOGIC_VECTOR(15 downto 0);
-			D_in				: in  STD_LOGIC_VECTOR(7 downto 0);
-			D_out				: out STD_LOGIC_VECTOR(7 downto 0);
-			RESET_n			: out STD_LOGIC;
-			
-			vdp_RD_n			: out STD_LOGIC;
-			vdp_WR_n			: out STD_LOGIC;
-			vdp_D_out		: in  STD_LOGIC_VECTOR(7 downto 0);
-			psg_WR_n			: out STD_LOGIC;
-			io_RD_n			: out STD_LOGIC;
-			io_WR_n			: out STD_LOGIC;
-			io_D_out			: in  STD_LOGIC_VECTOR(7 downto 0);
-			ram_RD_n			: out STD_LOGIC;
-			ram_WR_n			: out STD_LOGIC;
-			ram_D_out		: in  STD_LOGIC_VECTOR(7 downto 0);
-			rom_RD_n			: out STD_LOGIC;
-			rom_WR_n			: out STD_LOGIC;
-			rom_D_out		: in  STD_LOGIC_VECTOR(7 downto 0);
-			
-			boot_rom_RD_n	: out STD_LOGIC;
-			boot_rom_D_out	: in  STD_LOGIC_VECTOR(7 downto 0);
-			spi_RD_n			: out STD_LOGIC;
-			spi_WR_n			: out STD_LOGIC;
-			spi_D_out		: in  STD_LOGIC_VECTOR(7 downto 0);
-			uart_WR_n		: out STD_LOGIC;
-			uart_D_out		: in  STD_LOGIC_VECTOR(7 downto 0));
+   port (
+		CLKIN_IN			: in  std_logic; 
+		CLKIN_IBUFG_OUT: out std_logic;         
+		CLKFX_OUT 		: out std_logic;
+		CLKFX180_OUT	: out std_logic;
+		CLK2X_OUT		: out std_logic);
 	end component;
 	
 --	component dummy_z80 is
@@ -160,12 +128,13 @@ architecture Behavioral of main is
 	end component;
 
 	component ram is
-	port (clk:		in  STD_LOGIC;
-			RD_n:		in  STD_LOGIC;
-			WR_n:		in  STD_LOGIC;
-			A:			in  STD_LOGIC_VECTOR(12 downto 0);
-			D_in:		in  STD_LOGIC_VECTOR(7 downto 0);
-			D_out:	out STD_LOGIC_VECTOR(7 downto 0));
+	port (
+		clk:			in  STD_LOGIC;
+		RD_n:			in  STD_LOGIC;
+		WR_n:			in  STD_LOGIC;
+		A:				in  STD_LOGIC_VECTOR(12 downto 0);
+		D_in:			in  STD_LOGIC_VECTOR(7 downto 0);
+		D_out:		out STD_LOGIC_VECTOR(7 downto 0));
 	end component;
 
 	component boot_rom is
@@ -173,7 +142,6 @@ architecture Behavioral of main is
 		clk:			in  STD_LOGIC;
 		RD_n:			in  STD_LOGIC;
 		A:				in  STD_LOGIC_VECTOR(12 downto 0);
-		D_in:			in  STD_LOGIC_VECTOR(7 downto 0);
 		D_out:		out STD_LOGIC_VECTOR(7 downto 0));
 	end component;
 
@@ -236,7 +204,6 @@ architecture Behavioral of main is
 	signal ram_D_out		: std_logic_vector(7 downto 0);
 	
 	signal rom_RD_n		: std_logic;
-	signal rom_WR_n		: std_logic;
 	signal rom_D_out		: std_logic_vector(7 downto 0);
 	
 	signal spi_RD_n		: std_logic;
@@ -253,6 +220,20 @@ architecture Behavioral of main is
 
 	signal x: unsigned(8 downto 0);
 	signal y: unsigned(7 downto 0);
+	
+	
+	
+	
+
+	signal reset_counter:	unsigned(3 downto 0) := "1111";
+	signal bootloader:		std_logic := '0';
+	signal irom_D_out:		std_logic_vector(7 downto 0);
+	signal irom_RD_n:			std_logic := '1';
+	
+	signal RD_n_reg:			std_logic := '1';
+	signal WR_n_reg:			std_logic := '1';
+	signal RD_n_clk:			std_logic := '1';
+	signal WR_n_clk:			std_logic := '1';
 
 begin
 
@@ -270,39 +251,6 @@ begin
 			clk16 <= not clk16;
 		end if;
 	end process;
-	
-	glue_inst: glue 
-	port map(
-		clk				=> clk8,
-		IO_n				=> IO_n,
-		RD_n				=> RD_n,
-		WR_n				=> WR_n,
-		A					=> A,
-		D_in				=> D_in,
-		D_out				=> D_out,
-		RESET_n			=> RESET_n,
-			
-		vdp_RD_n			=> vdp_RD_n,
-		vdp_WR_n			=> vdp_WR_n,
-		vdp_D_out		=> vdp_D_out,
-		psg_WR_n			=> psg_WR_n,
-		io_RD_n			=> io_RD_n,
-		io_WR_n			=> io_WR_n,
-		io_D_out			=> io_D_out,
-		ram_RD_n			=> ram_RD_n,
-		ram_WR_n			=> ram_WR_n,
-		ram_D_out		=> ram_D_out,
-		rom_RD_n			=> rom_RD_n,
-		rom_WR_n			=> rom_WR_n,
-		rom_D_out		=> rom_D_out,
-			
-		boot_rom_RD_n	=> boot_rom_RD_n,
-		boot_rom_D_out	=> boot_rom_D_out,
-		spi_RD_n			=> spi_RD_n,
-		spi_WR_n			=> spi_WR_n,
-		spi_D_out		=> spi_D_out,
-		uart_WR_n		=> uart_WR_n,
-		uart_D_out		=> uart_D_out);
 	
 	
 --	z80_inst: dummy_z80
@@ -402,7 +350,6 @@ begin
 		clk			=> clk8,
 		RD_n			=> boot_rom_RD_n,
 		A				=> A(12 downto 0),
-		D_in			=> D_in,
 		D_out			=> boot_rom_D_out);
 	
 --	spi_inst: dummy_spi
@@ -429,6 +376,73 @@ begin
 		ready			=> uart_D_out(0));
 	
 	uart_D_out(7 downto 1) <= (others=>'0');
+	
+	
+	
+	
+	
+	-- glue logic	
+
+	reset_n <= '0' when reset_counter>0 else '1';
+
+	process (clk8)
+	begin
+		if rising_edge(clk8) then
+			if reset_counter>0 then
+				reset_counter <= reset_counter - 1;
+			end if;
+		end if;
+	end process;
+	
+	process (clk8)
+	begin
+		if rising_edge(clk8) then
+			RD_n_clk <= RD_n or not RD_n_reg;
+			WR_n_clk <= WR_n or not WR_n_reg;
+			RD_n_reg <= RD_n;
+			WR_n_reg <= WR_n;
+		end if;
+	end process;
+	
+	vdp_WR_n <= io_n or WR_n_clk or not A(7) or A(6);
+	vdp_RD_n <= io_n or RD_n_clk or not (A(7) xor A(6));
+	
+	psg_WR_n <= io_n or WR_n_clk or A(7) or not A(6);
+
+	io_WR_n <= io_n or WR_n_clk or A(7) or A(6) or not A(0);
+	io_RD_n <= io_n or RD_n_clk or not (A(7) and A(6));
+
+	spi_WR_n <= bootloader or io_n or WR_n_clk or not (A(7) and A(6));
+	spi_RD_n <= bootloader or io_n or RD_n_clk or A(7) or A(6);
+	
+	ram_WR_n <= not io_n or WR_n_clk or not (A(15) and A(14));
+	
+	process (clk8)
+	begin
+		if rising_edge(clk8) then
+			if WR_n_clk='0' then
+				if io_n='0' and A(7)='0' and A(6)='0' and A(0)='0' then
+					-- memory control
+					if bootloader='0' then
+						bootloader <= D_in(7);
+					end if;
+				end if;
+			end if;
+		end if;
+	end process;
+	
+	irom_D_out <= boot_rom_D_out when bootloader='0' else rom_D_out;
+	
+	with io_n&A select
+	D_out <= spi_D_out		when "0--------00------",
+				vdp_D_out		when "0--------01------",
+				vdp_D_out		when "0--------10------",
+				io_D_out			when "0--------11------",
+				irom_D_out		when "100--------------",
+				irom_D_out		when "101--------------",
+				irom_D_out		when "110--------------",
+				ram_D_out		when "111--------------",
+				(others=>'-')	when others;
 
 end Behavioral;
 
