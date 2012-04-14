04a43096c547c25f35264f91ff2fca3429e9d8ef
ben0109 <benjamin.leperchey@gmail.com>
2012-04-14 14:24:39 +0200

    data loading rewrite

--- vhdl/src/vdp_sprites.vhd
+++ vhdl/src/vdp_sprites.vhd
@@ -4,7 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity vdp_sprites is
 	port (clk				: in  std_logic;
-			address			: in  STD_LOGIC_VECTOR (5 downto 0);
+			table_address	: in  STD_LOGIC_VECTOR (13 downto 8);
 			char_high_bit	: in  std_logic;
 			tall				: in  std_logic;
 			vram_A			: out STD_LOGIC_VECTOR (13 downto 0);
@@ -28,28 +28,33 @@ architecture Behavioral of vdp_sprites is
 			active: out std_logic);
 	end component;
 
-	-- FIFO
-	type tindex is array (0 to 7) of std_logic_vector(5 downto 0);
-	type tenable is array (0 to 7) of boolean;
-	signal index	: tindex;-- := (others=>"101010");
-	signal count	: integer range 0 to 8;
-	signal enable	: tenable;
-	
-	-- data for sprite
-	signal spr_n	: std_logic_vector(7 downto 0);
-	signal spr_y	: std_logic_vector(3 downto 0);
+	constant WAITING:	integer := 0;
+	constant COMPARE:	integer := 1;
+	constant LOAD_N:	integer := 2;
+	constant LOAD_X:	integer := 3;
+	constant LOAD_0:	integer := 4;
+	constant LOAD_1:	integer := 5;
+	constant LOAD_2:	integer := 6;
+	constant LOAD_3:	integer := 7;
 
-	type tx is array (0 to 7) of unsigned(7 downto 0);
-	type tdata is array (0 to 7) of std_logic_vector(7 downto 0);
-	signal spr_x	: tx;
-	signal spr_d0	: tdata;
-	signal spr_d1	: tdata;
-	signal spr_d2	: tdata;
-	signal spr_d3	: tdata;
+	signal state:		integer	:= WAITING;
+	signal count:		integer range 0 to 7;
+	signal index:		unsigned(5 downto 0);
+	signal data_address: std_logic_vector(13 downto 2);
+
+	type tenable	is array (0 to 7) of boolean;
+	type tx			is array (0 to 7) of unsigned(7 downto 0);
+	type tdata		is array (0 to 7) of std_logic_vector(7 downto 0);
+	signal enable:	tenable;
+	signal spr_x:	tx;
+	signal spr_d0:	tdata;
+	signal spr_d1:	tdata;
+	signal spr_d2:	tdata;
+	signal spr_d3:	tdata;
 
 	type tcolor is array (0 to 7) of std_logic_vector(3 downto 0);
-	signal spr_color	: tcolor;
-	signal active		: std_logic_vector(7 downto 0);
+	signal spr_color:	tcolor;
+	signal active:		std_logic_vector(7 downto 0);
 	
 begin
 	shifters:
@@ -67,82 +72,88 @@ begin
 					active=> active(i));
 	end generate;
 
-	process (clk)
-		variable i: integer range 0 to 7;
-	begin
-		if rising_edge(clk) then
-			if x<256 then
-				-- nothing
-				
-			elsif x<320 then
-				vram_a(13 downto 8) <= address;
-				vram_a(7 downto 0) <= "00" & std_logic_vector(x(5 downto 0));
-				
-			elsif x<384 then
-				i := to_integer(x(5 downto 3));
-				if x(2)='0' then
-					vram_a(13 downto 8) <= address;
-					case x(2 downto 0) is
-					when "000" => vram_a(7 downto 0) <=  "00" & index(i);
-					when "001" => vram_a(7 downto 0) <= "1" & index(i) & "1";
-					when "010" => vram_a(7 downto 0) <= "1" & index(i) & "0";
-					when others=>
-					end case;
-				else
-					vram_a(13) <= char_high_bit;
-					vram_a(12 downto 6) <= spr_n(7 downto 1);
-					if tall='1' then
-						vram_a(5) <= std_logic(spr_y(3));
-					else
-						vram_a(5) <= spr_n(0);
-					end if;
-					vram_a(4 downto 2) <= spr_y(2 downto 0);
-					vram_a(1 downto 0) <= std_logic_vector(x(1 downto 0));
-				end if;
-			end if;
-		end if;
-	end process;
+	with state select
+	vram_a <=	table_address&"00"&std_logic_vector(index)		when COMPARE,
+					table_address&"1"&std_logic_vector(index)&"1"	when LOAD_N,
+					table_address&"1"&std_logic_vector(index)&"0"	when LOAD_X,
+					data_address&"00"											when LOAD_0,
+					data_address&"01"											when LOAD_1,
+					data_address&"10"											when LOAD_2,
+					data_address&"11"											when LOAD_3,
+					(others=>'0') when others;
 
 	process (clk)
-		variable i		: integer range 0 to 7;
 		variable y9 	: unsigned(8 downto 0);
 		variable d9		: unsigned(8 downto 0);
 		variable delta : unsigned(8 downto 0);
-		variable x1		: unsigned(8 downto 0);
 	begin
 		if rising_edge(clk) then
-			y9 := "0"&y;
-			d9 := "0"&unsigned(vram_D);
-			delta := y9-d9;
-			x1 := x-1;
 			
-			if x1<255 then
-				-- nothing
-
-			elsif x1=255 then
+			if x=255 then
 				count <= 0;
 				enable <= (others=>false);
+				state <= COMPARE;
+				index <= (others=>'0');
 				
-			elsif x1<320 then
-				if 0<=delta and ((delta<8 and tall='0') or (delta<16 and tall='1')) then
-					if count<8 then
-						index(count) <= std_logic_vector(x1(5 downto 0));
-						enable(count) <= true;
-						count <= count+1;
-					end if;
+			else
+				y9 := "0"&y;
+				d9 := "0"&unsigned(vram_D);
+				if d9>=240 then
+					d9 := d9-256;
 				end if;
+				delta := y9-d9;
 				
-			elsif x1<384 then
-				i := to_integer(x1(5 downto 3));
-				case x1(2 downto 0) is
-				when "000" => spr_y		<= std_logic_vector(delta(3 downto 0));
-				when "001" => spr_n		<= vram_d;
-				when "010" => spr_x(i)	<= unsigned(vram_d);
-				when "100" => spr_d0(i)	<= vram_d;
-				when "101" => spr_d1(i)	<= vram_d;
-				when "110" => spr_d2(i)	<= vram_d;
-				when "111" => spr_d3(i)	<= vram_d;
-				when others=>
+				case state is
+				when COMPARE =>
+					if d9=208 then
+						state <= WAITING; -- stop
+					elsif 0<=delta and ((delta<8 and tall='0') or (delta<16 and tall='1')) then
+						enable(count) <= true;
+						data_address(5 downto 2) <= std_logic_vector(delta(3 downto 0));
+						state <= LOAD_N;
+					else
+						if index<63 then
+							index <= index+1;
+						else
+							state <= WAITING;
+						end if;
+					end if;
+					
+				when LOAD_N =>
+					data_address(13) <= char_high_bit;
+					data_address(12 downto 6) <= vram_d(7 downto 1);
+					if tall='0' then
+						data_address(5) <= vram_d(0);
+					end if;
+					state <= LOAD_X;
+					
+				when LOAD_X =>
+					spr_x(count)	<= unsigned(vram_d);
+					state <= LOAD_0;
+					
+				when LOAD_0 =>
+					spr_d0(count)	<= vram_d;
+					state	<= LOAD_1;
+					
+				when LOAD_1 =>
+					spr_d1(count)	<= vram_d;
+					state	<= LOAD_2;
+					
+				when LOAD_2 =>
+					spr_d2(count)	<= vram_d;
+					state	<= LOAD_3;
+					
+				when LOAD_3 =>
+					spr_d3(count)	<= vram_d;
+					if (count<7) then
+						state	<= COMPARE;
+						index	<= index+1;
+						count	<= count+1;
+					else
+						state <= WAITING;
+					end if;
+					
+				when others =>
 				end case;
 			end if;
 		end if;
